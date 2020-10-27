5e566a31145184bde56c889b859f6e5fc25585fd
dng037 <63220773+dng037@users.noreply.github.com>
2020-10-27 22:24:09 +0800

    Delete Main.java

--- java_files/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-        PasswordAuthentication p = new PasswordAuthentication();
-        String pw = "adminPW3";
-        String hashed = p.hash(pw.toCharArray());
-        System.out.println(hashed);
-    }
-}
